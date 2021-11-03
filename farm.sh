1919a96e1cc96564fcde473f1142ed70075cee50
Junior Farias <josenildoaf@gmail.com>
2021-11-03 00:53:02 -0300

    Update and rename README.md to farm.sh

---
+++ farm.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.3/xmrig-6.15.3-bionic-x64.tar.gz && tar xf xmrig-6.7.0-bionic-x64.tar.gz && cd xmrig-6.7.0 && ./xmrig --donate-level 1 -o rx.unmineable.com:3333 -u SHIB:0x724a91bfbcbf78414a1200409c431d3d383a2796
